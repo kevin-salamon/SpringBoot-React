eb304816a94e6eb6aaaa721e9efa704b9e1d752e
kevin-salamon <kevinwebdevs@gmail.com>
2021-08-17 20:56:38 -0400

    initial setup for auth

--- workouts/frontend/src/Pages/Login/Login.js
+++ workouts/frontend/src/Pages/Login/Login.js
@@ -8,17 +8,56 @@ const StyledLogin = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    background-color: #C792DF;
+    position: relative;
+
+    .login-container {
+        height: 70%;
+        width: 60%;
+        background-color: white;
+        border-radius: 12px;
+        box-shadow: 3px 3px 8px 7px #3b3b3b40;
+        display: grid;
+        grid-template-columns: 3fr 5fr;
+
+        .form-container {
+            grid-column: 1 / span 1;
+            position: relative;
+        }
+
+        .design-container {
+            grid-column: 2 / span 1;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            .design-box {
+                width: 95%;
+                height: 95%;
+                border-radius: 12px;
+                background-color: #6CD4FF;
+                box-shadow: 1px 1px 3px 1px #3b3b3b80;
+            }
+        }
+    }
 `;
 
-const Login = ({
-    pageSelected
-}) => {
+const Login = () => {
 
     return (
         <>
             {/* <Header/> */}
             <StyledLogin>
-                <p>{pageSelected}</p>
+                <div className='login-container'>
+                    <div className='form-container'>
+
+                    </div>
+                    <div className='design-container'>
+                        <div className='design-box'>
+
+                        </div>
+                    </div>
+                </div>
             </StyledLogin>
         </>
     )
